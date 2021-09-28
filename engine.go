f7b2223aae48f653a745b40a3e5f47818f3c1661
Matheus Rosa <matheus.alves.rosa@outlook.com>
2021-09-27 23:20:04 -0300

    code review

--- engine.go
+++ engine.go
@@ -1,6 +1,8 @@
 package go_factory
 
-import "reflect"
+import (
+	"reflect"
+)
 
 type Factory struct {
 	opts   *Options
@@ -12,27 +14,17 @@ type Options struct {
 	// gorm *DB
 }
 
-type Fields map[string]interface{}
-
-func (f Fields) String(fieldName, defaultValue string) string {
-	if _, ok := f[fieldName]; ok {
-		return f[fieldName].(string)
-	}
-
-	return defaultValue
-}
-
 func NewFactory(opts *Options) *Factory {
 	return &Factory{opts: opts}
 }
 
-func (f *Factory) Create(model string, fields Fields) interface{} {
+func (f Factory) Create(model string, fields Fields) interface{} {
 	f.fields = fields
 	stub := f.opts.BaseFactory()[model]
 	fun := reflect.ValueOf(stub)
 
 	in := make([]reflect.Value, 1)
-	in[0] = reflect.ValueOf(fields)
+	in[0] = reflect.ValueOf(f)
 	res := fun.Call(in)
 
 	return res[0].Interface()
