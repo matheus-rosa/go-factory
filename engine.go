4c626405f16b08db1b19f9b8eba1be78c1c6c9a4
Matheus Rosa <matheus.alves.rosa@outlook.com>
2021-10-11 15:13:33 -0300

    added Create, CreateN, Insert and InsertN methods

--- engine.go
+++ engine.go
@@ -12,15 +12,20 @@ type Factory struct {
 
 type Options struct {
 	BaseFactory func() map[string]interface{}
-	gorm        *gorm.DB
+	Gorm        *gorm.DB
 }
 
 func NewFactory(opts *Options) *Factory {
 	return &Factory{opts: opts}
 }
 
-func (f Factory) Create(model string, fields Fields) interface{} {
-	f.fields = fields
+func (f Factory) Create(model string, fields ...Fields) interface{} {
+	if len(fields) > 0 {
+		f.fields = fields[0]
+	} else {
+		f.fields = Fields{}
+	}
+
 	stub := f.opts.BaseFactory()[model]
 	fun := reflect.ValueOf(stub)
 
@@ -31,8 +36,39 @@ func (f Factory) Create(model string, fields Fields) interface{} {
 	return res[0].Interface()
 }
 
-func (f Factory) Insert(model string, fields Fields) interface{} {
-	m := f.Create(model, fields)
+func (f Factory) CreateN(model string, output interface{}, fields ...Fields) {
+	v := reflect.ValueOf(output)
+	n := v.Elem().Len()
+	fieldsLen := len(fields)
 
-	return f.opts.gorm.Create(m).Error
+	for i := 0; i < n; i++ {
+		var modelCreated interface{}
+		if i < fieldsLen {
+			modelCreated = f.Create(model, fields[i])
+		} else {
+			modelCreated = f.Create(model)
+		}
+
+		modelCreatedRfl := reflect.ValueOf(modelCreated)
+		if v.Elem().Index(i).Kind() == reflect.Ptr {
+			v.Elem().Index(i).Set(modelCreatedRfl)
+			continue
+		}
+
+		currentStruct := v.Elem().Index(i)
+		for j := 0; j < currentStruct.NumField(); j++ {
+			currentStruct.Field(j).Set(modelCreatedRfl.Elem().Field(j))
+		}
+	}
+}
+
+func (f Factory) Insert(model string, fields ...Fields) interface{} {
+	m := f.Create(model, fields...)
+
+	return f.opts.Gorm.Create(m).Error
+}
+
+func (f Factory) InsertN(model string, output interface{}, fields ...Fields) interface{} {
+	f.CreateN(model, output, fields...)
+	return f.opts.Gorm.Create(output).Error
 }
